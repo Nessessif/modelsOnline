47cad2883e7c3e288e262810ef832701d596f26c
Nessessif <kovpen@github.com>
2021-02-26 02:27:55 +0300

    add nmp packages

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
-window.onload = () => {
-    const scene = THREE.Scene();
-    let camera = new ThREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 3000);
-    let render = new THREE.WebGLRenderer({ antialias: true });
+window.onload = function () {
+
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 10000);
+
+    var render = new THREE.WebGLRenderer({ antialias: true });
     render.setSize(window.innerWidth, window.innerHeight);
     render.setClearColor(0x778899);
+
     document.body.appendChild(render.domElement);
+
     camera.position.z = 250;
-    let light = new THREE.DirectionalLight(0xfff7e8, 1);
+
+    var light = new THREE.DirectionalLight(0xfff7e8, 1);
     scene.add(light);
-    const ambColor = '#faffe3';
-    const ambLight = new THREE.AmbientLight(ambColor);
+
+    var ambColor = '#faffe3';
+    var ambLight = new THREE.AmbientLight(ambColor);
+    
     scene.add(ambLight);
-    let manager = new THREE.LoadingManager();
-    let loader = new THREE.ImageLoader(manager);
-    let texture = new THREE.Texture();
-    loader.load('/models/striped_shoe_0.jpg', (image) => {
+
+    var manager = new THREE.LoadingManager();
+    var loader = new THREE.ImageLoader(manager);
+
+    var texture = new THREE.Texture();
+
+    loader.load('model/striped_shoe_0.jpg', function (image) {
         texture.image = image;
         texture.needsUpdate = true;
     });
@@ -24,7 +34,7 @@ window.onload = () => {
 
     var changing;
 
-    objLoader.load('/models/striped_shoe.obj', function (object) {
+    objLoader.load('model/striped_shoe.obj', function (object) {
         object.traverse(function (child) {
             if (child instanceof THREE.Mesh) {
                 meshes.push(child);
@@ -53,25 +63,41 @@ window.onload = () => {
     }
 
     rendering();
-}
+};
 
 
 
-    // var scene = new THREE.Scene();
-    // var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    // var renderer = new THREE.WebGLRenderer();
-    // renderer.setSize(window.innerWidth, window.innerHeight);
-    // document.body.appendChild(renderer.domElement);
-    // var geometry = new THREE.BoxGeometry(10, 10, 10);
-    // var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    // var cube = new THREE.Mesh(geometry, material);
-    // scene.add(cube);
+// var scene = new THREE.Scene();
+// var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// var renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
 
-    // camera.position.z = 25;
-    // function render() {
-    //     requestAnimationFrame(render);
-    //     cube.rotation.x += 0.01;
-    //     cube.rotation.y += 0.01;
-    //     renderer.render(scene, camera);
-    // }
-    // render();
+// var geometry = new THREE.BoxGeometry(10, 10, 10);
+// var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// var cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+// camera.position.z = 25;
+// function render() {
+//     requestAnimationFrame(render);
+//     cube.rotation.x += 0.01;
+//     cube.rotation.y += 0.01;
+//     renderer.render(scene, camera);
+// }
+// render();
+
+
+// // var geometry = new THREE.SphereGeometry(1, 32, 32);
+// // var material = new THREE.MeshNormalMaterial();
+// // var sphere = new THREE.Mesh(geometry, material);
+// // scene.add(sphere);
+// // camera.position.z = 3;
+
+// // function render() {
+// //     requestAnimationFrame(render);
+// //     sphere.rotation.x += 0.01;
+// //     sphere.rotation.y += 0.01;
+// //     renderer.render(scene, camera);
+// // }
+// // render();
+
